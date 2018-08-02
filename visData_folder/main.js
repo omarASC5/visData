41fba86ab99947e2327e99beb58003011d2599e0
omarASC5 <ocobas19@avenues.org>
2018-08-02 13:41:51 -0400

    basic networking app complete!

---
+++ visData_folder/main.js
@@ -0,0 +1,54 @@
+for (let i = 0; i < userData.results.length; i++) {
+    const userDiv = document.createElement('div');
+    const state_paragraph = document.createElement('p');
+    const add_image = document.createElement('img');
+    const name_paragraph = document.createElement('p');
+    const email_paragraph = document.createElement('p');
+    const city_paragraph = document.createElement('p');
+
+   
+
+
+    document.body.appendChild(userDiv);
+    let divs = document.getElementsByTagName('div');
+    userDiv.appendChild(state_paragraph);
+    userDiv.appendChild(add_image);
+    userDiv.appendChild(name_paragraph);
+    userDiv.appendChild(email_paragraph);
+    userDiv.appendChild(city_paragraph);
+
+    state_paragraph.classList.add("person_state");
+    add_image.classList.add("person_image");
+    name_paragraph.classList.add("person_name");
+    email_paragraph.classList.add("person_email");
+    city_paragraph.classList.add("person_city");
+    
+
+
+    //Add Classes to each Div
+    userDiv.classList.add("person");
+    // userDiv.classList.add("person_name");
+    // userDiv.classList.add("person_email");
+    // userDiv.classList.add("person_city");
+
+const person_state = document.getElementsByClassName('person_state');
+const person_image = document.getElementsByClassName('person_image');
+const person_name = document.getElementsByClassName('person_name');
+const person_email = document.getElementsByClassName('person_email');
+const person_city = document.getElementsByClassName('person_city');
+
+const person_state_uppercase = titleCase(userData.results[i].location.state);
+person_state[i].innerHTML = "State: " + person_state_uppercase;
+
+person_image[i].src = userData.results[i].picture.large;
+
+person_name_uppercase = titleCase((userData.results[i].name.title + ". " + userData.results[i].name.first + " " + userData.results[i].name.last));
+
+person_name[i].innerHTML = "Name: " + person_name_uppercase;
+
+person_email[i].innerHTML = "Email: " + userData.results[i].email;
+
+const person_city_uppercase = titleCase(userData.results[i].location.city)
+person_city[i].innerHTML = "City: " + person_city_uppercase;
+
+}
